4fe7bed339d1a655889a8031f5a8a06919bac7c1
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-03 21:37:54 -0500

    Added queryselectors for JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // JS for processing data items hw
+
+var s_one, s_two, s_three, s_four, s_five, s_six;
+
+s_one = document.querySelector('stuff_one');
+s_two = document.querySelector('stuff_two');
+s_three = document.querySelector('stuff_three');
+s_four = document.querySelector('stuff_four');
+s_five = document.querySelector('stuff_five');
+s_six = document.querySelector('stuff_six');
